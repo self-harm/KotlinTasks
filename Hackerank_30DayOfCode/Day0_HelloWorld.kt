328acf55381b16d20370e4477b81734fbd09f1b3
self-harm <lightgraywrite@gmail.com>
2021-05-26 22:27:12 +0300

    Create Day0_HelloWorld.kt

---
+++ Hackerank_30DayOfCode/Day0_HelloWorld.kt
@@ -0,0 +1,12 @@
+
+fun main(args: Array<String>) {
+    var words = readLine()!!.toString()
+    println("Hello, World!\n$words")
+}
+
+/*Sample Input:
+Welcome to 30 Days of Code!
+
+Sample Output:
+Hello, World. 
+Welcome to 30 Days of Code!*/
