007895b066879567036148e8105327abe33addd2
self-harm <lightgraywrite@gmail.com>
2021-06-02 22:58:14 +0300

    Create MastermindFun.kt

---
+++ Coursera/Week2/MastermindFun.kt
@@ -0,0 +1,19 @@
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+
+    val rightPositions = secret.zip(guess).count { TODO() }
+
+    val commonLetters = "ABCDEF".sumBy { ch ->
+
+        Math.min(secret.count { TODO() }, guess.count { TODO() })
+    }
+    return Evaluation(rightPositions, commonLetters - rightPositions)
+}
+
+fun main(args: Array<String>) {
+    val result = Evaluation(rightPosition = 1, wrongPosition = 1)
+    evaluateGuess("BCDF", "ACEB") eq result
+    evaluateGuess("AAAF", "ABCA") eq result
+    evaluateGuess("ABCA", "AAAF") eq result
+}
