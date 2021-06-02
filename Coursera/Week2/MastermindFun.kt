b10a67f4c00019ae3df4611d27b990204f64e363
self-harm <lightgraywrite@gmail.com>
2021-06-02 23:22:19 +0300

    Update MastermindFun.kt

--- Coursera/Week2/MastermindFun.kt
+++ Coursera/Week2/MastermindFun.kt
@@ -5,7 +5,6 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
     val rightPositions = secret.zip(guess).count { it.first == it.second }
 
     val commonLetters = "ABCDEF".sumBy { ch ->
-
         Math.min(secret.count { it == ch }, guess.count { it == ch })
     }
     return Evaluation(rightPositions, commonLetters - rightPositions)
