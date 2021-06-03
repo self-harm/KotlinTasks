2eba641971a1c6c1a2f9d0c813edd6ec092d3270
self-harm <lightgraywrite@gmail.com>
2021-06-03 11:45:06 +0300

    Update NiceString.kt

--- Coursera/Week3/NiceString.kt
+++ Coursera/Week3/NiceString.kt
@@ -1,5 +1,27 @@
 package nicestring
 
 fun String.isNice(): Boolean {
-    TODO()
+    var firstCondition = true
+    var secondCondition = false
+    var thirdCondition = false
+
+    var counter = 0
+    val setOfVowels = setOf('a', 'e', 'i', 'o', 'u')
+    val listOfPairs = this.zipWithNext()
+
+    for (ch in this) {
+        if (setOfVowels.contains(ch)) counter++
+    }
+
+    for (pair in listOfPairs) {
+        if ( pair.first == 'b' && (pair.second == 'u' || pair.second == 'a' || pair.second == 'e')) firstCondition =
+            false
+        if (any { pair.first == pair.second }) secondCondition = true
+    }
+
+    if (counter >= 3) thirdCondition = true
+
+    return (firstCondition && secondCondition)
+        || (firstCondition && thirdCondition)
+        || (secondCondition && thirdCondition)
 }
