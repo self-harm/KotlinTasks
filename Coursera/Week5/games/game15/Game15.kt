83123d5e6e5f21b505a3a0862aa645e7c8360118
self-harm <lightgraywrite@gmail.com>
2021-06-08 17:24:35 +0300

    Create Game15.kt

---
+++ Coursera/Week5/games/game15/Game15.kt
@@ -0,0 +1,84 @@
+package games.gameOfFifteen
+
+import board.Direction
+import board.GameBoard
+import board.createGameBoard
+import games.game.Game
+
+/*
+ * Implement the Game of Fifteen (https://en.wikipedia.org/wiki/15_puzzle).
+ * When you finish, you can play the game by executing 'PlayGameOfFifteen'.
+ */
+fun newGameOfFifteen(initializer: GameOfFifteenInitializer = RandomGameInitializer()): Game {
+    return object : Game {
+        val board = createGameBoard<Int?>(4)
+
+        override fun initialize() {
+            board.initBoard(initializer)
+        }
+
+        override fun canMove(): Boolean = board.any { it == null }
+
+        override fun hasWon(): Boolean {
+            val cells = board.getAllCells()
+
+            for (i in 1..15) {
+                if (board[cells.elementAt(i - 1)] != i) return false
+            }
+
+            return true
+        }
+
+        override fun processMove(direction: Direction) {
+            val nullCell = board.filter { it == null }.first()
+
+            with(board) {
+                when (direction) {
+                    Direction.UP -> {
+                        if (board.getCellOrNull(nullCell.i + 1, nullCell.j) != null) {
+                            val t = get(getCell(nullCell.i + 1, nullCell.j))
+                            set(nullCell, t)
+                            set(getCell(nullCell.i + 1, nullCell.j), null)
+                        }
+                    }
+                    Direction.DOWN -> {
+                        if (getCellOrNull(nullCell.i - 1, nullCell.j) != null) {
+                            val t = get(getCell(nullCell.i - 1, nullCell.j))
+                            set(nullCell, t)
+                            set(getCell(nullCell.i - 1, nullCell.j), null)
+                        }
+                    }
+                    Direction.RIGHT -> {
+                        if (getCellOrNull(nullCell.i, nullCell.j - 1) != null) {
+                            val t = get(getCell(nullCell.i, nullCell.j - 1))
+                            set(nullCell, t)
+                            set(getCell(nullCell.i, nullCell.j - 1), null)
+                        }
+                    }
+                    Direction.LEFT -> {
+                        if (getCellOrNull(nullCell.i, nullCell.j + 1) != null) {
+                            val t = get(getCell(nullCell.i, nullCell.j + 1))
+                            set(nullCell, t)
+                            set(getCell(nullCell.i, nullCell.j + 1), null)
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun get(i: Int, j: Int): Int? = board[board.getCell(i, j)]
+
+        fun GameBoard<Int?>.initBoard(initializer: GameOfFifteenInitializer) {
+            val allCells = this.getAllCells()
+            val p = initializer.initialPermutation
+
+            for (i in allCells.indices) {
+                try {
+                    this[allCells.elementAt(i)] = p[i]
+                } catch (e: Exception) {
+                    this[allCells.elementAt(i)] = null
+                }
+            }
+        }
+    }
+}
